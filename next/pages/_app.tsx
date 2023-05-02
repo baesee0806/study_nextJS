04cb4d597cc131af3b5ae78ea254156ddf8a632a
배성완 <baesee@kakao.com>
2023-05-02 22:02:41 +0900

    기본 구조 및 기능

--- next/pages/_app.tsx
+++ next/pages/_app.tsx
@@ -1,6 +1,16 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <div>
+      <Head>
+        <title>테스트 입니다</title>
+      </Head>
+      <div>Header</div>
+      <Component {...pageProps} />
+      <div>Footer</div>
+    </div>
+  )
 }
