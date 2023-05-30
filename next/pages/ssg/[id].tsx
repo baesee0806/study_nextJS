c4b29acd26f853e0c5fd8d146b66ca46b95245ca
배성완 <baesee@kakao.com>
2023-05-30 15:18:34 +0900

    ssg getStaticPaths

--- next/pages/ssg/[id].tsx
+++ next/pages/ssg/[id].tsx
@@ -13,8 +13,22 @@ const Post = ({post}:any) => {
 
 export default Post;
 
+export async function getStaticPaths(){
+    return{
+        paths:[
+            {params:{id:'1'}},
+            {params:{id:'2'}},
+            {params:{id:'3'}},
+        ],
+        fallback:false,
+    };
+}
+
 export async function getStaticProps(context:any){
-    const response = await fetch(`https://jsonplaceholder.typicode.com/posts/2`);
+   
+    const {id}  = context.params;
+
+    const response = await fetch(`https://jsonplaceholder.typicode.com/posts/${id}`);
     const post = await response.json();
 
     return{
